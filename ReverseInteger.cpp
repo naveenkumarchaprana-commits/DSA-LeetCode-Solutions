c5aacc5c660860935ff1679db98ff991b5e3f14b
naveenkumarchaprana-commits <naveenkumarchaprana@gmail.com>
2026-07-24 09:38:46 +0530

    Added Reverse Integer solution

---
+++ ReverseInteger.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rem, ans = 0;
+        
+        while (x != 0) {
+            rem = x % 10;
+            x /= 10;
+
+            // Overflow check
+            if (ans > INT_MAX / 10 || ans < INT_MIN / 10) {
+                return 0;
+            }
+
+            ans = ans * 10 + rem;
+        }
+        
+        return ans;
+    }
+};
