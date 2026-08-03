453f0ded7600083f16c932e2d3273e3f38c2745e
naveenkumarchaprana-commits <naveenkumarchaprana@gmail.com>
2026-08-03 13:18:09 +0530

    Add array reversal functionality
    Implement array reversal using a while loop and swap function.

---
+++ Reverse-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    {
+        int arr[6] = {2,3,4,5,6};
+        int start=0,end=4;
+        while(start<end)
+        {
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+        };
+        for(int i=0;i<6;i++)
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
